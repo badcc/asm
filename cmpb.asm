251a72511c1505cef999068f39dc1646b83d46e1
badcc <lexbalf@gmail.com>
2015-09-19 16:27:50 -0400

    Update cmpb.asm

--- cmpb.asm
+++ cmpb.asm
@@ -18,11 +18,3 @@ main:
 
 	pop rbp
 	ret
-
-; Calling convention
-; RDI, RSI, RDX, RCX
-; R0  R1  R2  R3  R4  R5  R6  R7  R8  R9  R10  R11  R12  R13  R14  R15
-; RAX RCX RDX RBX RSP RBP RSI RDI
-; printf is varargs, set rax to 0 before call.
-; Return in eax
-; sub rsp by number of rbp that you use.
